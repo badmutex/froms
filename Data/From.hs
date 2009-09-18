ae6225b855653a787ba1bfa33b47abd9e18319d1
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-09-17 22:28:50 -0400

    FromTagged was redundant

--- Data/From.hs
+++ Data/From.hs
@@ -10,28 +10,22 @@ module Data.From where
 -- > data Foo = Foo String
 -- > instance From Foo String where
 -- >     from (Foo s) = s
-
-class From a b | a -> b where
-    from :: a -> b
-
-
--- | A more flexible version of 'From' using phantom types
---
--- > data Foo a = Foo Int | Bar String
+-- >
+-- > ---------------------------------------------
 -- >
 -- > data IntT
 -- > data StringT
 -- >
--- > instance FromTagged (Foo IntT) Int where
--- >     fromTagged (Foo a) = a
+-- > instance From (Foo IntT) Int where
+-- >     From (Foo a) = a
 -- >
--- > instance FromTagged (Foo StringT) String where
--- >     fromTagged (Bar a) = a
+-- > instance From (Foo StringT) String where
+-- >     From (Bar a) = a
 -- >
 -- > test = let foo = Foo 42      :: Foo IntT
 -- >            bar = Bar "hello" :: Foo StringT
--- >        in (fromTagged foo, fromTagged bar)
+-- >        in (from foo, from bar)
 -- > =====> (42,"hello")
 
-class FromTagged a b | a -> b where
-    fromTagged :: a -> b
+class From a b | a -> b where
+    from :: a -> b
